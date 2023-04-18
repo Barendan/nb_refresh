6c9e1407cbbe94fbf28a6bb9f8a9ba1691c66468
Barendan <barendan@gmail.com>
2023-04-18 18:34:31 -0400

    clean out public dir, add nav, and add logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+import Link from 'next/link';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css';
+import classes from './index.css';
+
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+        <Link href="/">
+            <div className={classes.logo}>Namblog</div>
+        </Link>
+        <nav>
+            <ul>
+                <li><Link href="/posts">Posts</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
 }
+
+const Layout = ( props ) => {
+  return (
+    <>
+      <MainNavigation />
+      <main> {props.children} </main>
+    </>
+  )
+}
+
+export default App = ({ Component, pageProps }) => (
+  <Layout>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+  </Layout>
+)
